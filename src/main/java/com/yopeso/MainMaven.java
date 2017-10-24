080bef094d9b6a9a09356703547ff97509ef966a
jarekblaszczykyopeso <jaroslaw.blaszczyk@yopeso.com>
2017-10-24 14:25:12 +0300

    init

---
+++ src/main/java/com/yopeso/MainMaven.java
@@ -0,0 +1,7 @@
+package com.yopeso;
+
+public class MainMaven {
+    public static void main (String[] aaa){
+        System.out.println("just the test from MainMaven");
+    }
+}
---
+++ src/test/java/com/yopeso/MainMavenTest.java
@@ -0,0 +1,14 @@
+package com.yopeso;
+
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MainMavenTest {
+
+    @Test
+    public void samppleTest() {
+        assertEquals(3, 3);
+    }
+}
